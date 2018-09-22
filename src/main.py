21d74fb4d616cb05e247e1c4ace72fc2d466132a
root <abhishek_official@hotmail.com>
2018-09-22 17:25:03 -0400

    Multi-threading

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import type
 from crawler import Crawl
+import threading
+import time
 
 target_url = 'http://10.0.2.6/mutillidae/'
 payload_file = 'xss_payloads.txt'
@@ -7,6 +9,22 @@ payload_file = 'xss_payloads.txt'
 crawler = Crawl(target_url)
 list_of_urls = crawler.getList()
 
-for url in list_of_urls:
-    xss_obj = type.XSS(url,payload_file)
+task_list = []
+
+def startThread(url):
+    xss_obj = type.XSS(url, payload_file)
     xss_obj.initiateEngine()
+
+t1 = time.time()
+
+for url in list_of_urls:
+    # t = threading.Thread(target=startThread, args=(url,))
+    # t.start()
+    # task_list.append(t)
+    startThread(url)
+
+# for t in task_list:
+#     t.join()
+
+t2 = time.time()
+print('The time taken is {}'.format(t2-t1))
--- src/scanner.py
+++ src/scanner.py
@@ -3,6 +3,9 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import time
 
 class Scanner:
 	def __init__(self,url,payload):
@@ -17,28 +20,44 @@ class Scanner:
 
 	def inject_payload(self):
 		list_forms = self.extract_forms()
+		list_of_tasks = []
+
+		#t1 = time.time()
 
 		for form in list_forms:
-			input_box = form.findAll('input')
-			post_data = {}
+			self.scanLoad(form)
+			# t = threading.Thread(target=self.scanLoad, args=(form,))
+			# t.start()
+			# list_of_tasks.append(t)
 
-			for i in range(len(self.payload)):
-				for box in input_box:
-					box_name = box.get('name')
-					type_box = box.get('type')
-					input_value = box.get('value')
-					if type_box == 'text':
-						input_value = self.payload[i]
+		# for task in list_of_tasks:
+		# 	task.join()
 
-					post_data[box_name]=input_value
+		#t2 = time.time()
 
-				result = requests.post(self.target_url,data=post_data)
+		#print('[!] Completed in {}'.format(t2-t1))
 
-				if self.payload[i] in result.text:
-					print('\n[!] VULNERABILITY DETECTED!--> ' + self.payload[i])
-					print('[*] LINK IS ',self.target_url)
-					print('---FORM DATA---')
-					print(form)
-					print('\n')
-				else:
-					print("[+] OK. \n")
+	def scanLoad(self, form):
+		input_box = form.findAll('input')
+		post_data = {}
+
+		for i in range(len(self.payload)):
+			for box in input_box:
+				box_name = box.get('name')
+				type_box = box.get('type')
+				input_value = box.get('value')
+				if type_box == 'text':
+					input_value = self.payload[i]
+
+				post_data[box_name]=input_value
+
+			result = requests.post(self.target_url,data=post_data)
+
+			if self.payload[i] in result.text:
+				print('\n[!] VULNERABILITY DETECTED!--> ' + self.payload[i])
+				print('[*] LINK IS ',self.target_url)
+				print('---FORM DATA---')
+				print(form)
+				print('\n')
+			else:
+				print("[+] OK. \n")
