788845e825ff965c69923bfbcd3353cc20ec494a
root <abhishek_official@hotmail.com>
2018-11-18 04:49:38 -0500

    updated fuzzer

--- src/fuzzer/fuzzer.py
+++ src/fuzzer/fuzzer.py
@@ -4,48 +4,55 @@ import queue
 import time
 import threading
 
+discovered_url = []
 
 def generate_url(base_url, fuzz_text):
-    new_url = base_url + '/' + fuzz_text
-    return new_url
+    return urljoin(base_url, fuzz_text)
+
 
 def readFromFile(file_path):
-    myQueue = queue.Queue()
+    fuzz_queue = queue.Queue()
     with open(file_path) as file:
         for url in file.readlines():
-            myQueue.put(url)
-    return myQueue
+            fuzz_queue.put(url)
+    return fuzz_queue
+
 
 def send_request(url):
     resp = requests.get(url)
     if resp.status_code == 200:
         return True
 
+
 def startEngine():
+    queue_generated = readFromFile('fuzz_url.txt')
     while not queue_generated.empty():
         new_url = generate_url('http://10.0.2.6/mutillidae', queue_generated.get())
         try:
             if send_request(new_url):
                 print('[+] ', new_url)
-                print(threading.current_thread())
+                discovered_url.append(url)
+                #print(threading.current_thread())
         except:
             pass
 
 
+
 if __name__ == '__main__':
-    queue_generated = readFromFile('fuzz_url.txt')
 
     t1 = time.time()
 
-    threads = []
+    # threads = []
+    #
+    # for i in range(10):
+    #     thread = threading.Thread(target=startEngine)
+    #     thread.start()
+    #     threads.append(thread)
+    #
+    # for thread in threads:
+    #     thread.join()
 
-    for i in range(10):
-        thread = threading.Thread(target=startEngine)
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
+    startEngine()
 
     t2 = time.time()
 
