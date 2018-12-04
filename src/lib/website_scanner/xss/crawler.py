492956bfe034677f74b0ee157197d56dc7105576
root <abhishek_official@hotmail.com>
2018-12-04 04:53:48 -0500

    xss vuln added

---
+++ src/lib/website_scanner/xss/crawler.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python
+
+import re
+from urllib.parse import *
+import requests
+
+
+class Crawl(object):
+
+    def __init__(self, url):
+        self.url = url
+        self.target_links = []
+        self.session = requests.Session()
+
+    def extract_links_from(self):
+        response = self.session.get(self.url)
+        return re.findall('(?:href=")(.*?)"', response.text)
+
+    def crawl(self, url=None):
+        if url == None:
+            url = self.url
+
+        href_links = self.extract_links_from()
+        self.target_links.append(url)
+
+        for link in href_links:
+            link = urljoin(url, link)
+
+            if '#' in link:
+                link = link.split('#')[0]
+
+            if self.url in link and link not in self.target_links:
+                if '.css' not in link and '.ico' not in link:
+                    self.target_links.append(link)
+                    self.crawl(link)
+
+    def getList(self):
+        print('[+] Crawling, collecting links...')
+        self.extract_links_from()
+        self.crawl()
+        return self.target_links
---
+++ src/lib/website_scanner/xss/xss.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+
+import scanner
+from crawler import Crawl
+
+
+class XSS(object):
+
+    def __init__(self, url, payload_file):
+        self.url = url
+        self.payload = payload_file
+        self.payload_list = []
+
+    def processPayload(self):
+
+        """This function process payload from file"""
+
+        with open(self.payload) as file:
+            for line in file.readlines():
+                line = line.strip()
+                yield line
+
+    def listPayloads(self):
+        for payload in self.processPayload():
+            self.payload_list.append(payload)
+
+        return self.payload_list
+
+    def initiateEngine(self):
+        print('[+] XSS Vulnerability Engine started...')
+        self.payload_list = self.listPayloads()
+        engine = scanner.Scanner(self.url, self.payload_list)
+        engine.inject_payload()
+
+
+if __name__  == '__main__':
+    newObj = Crawl(url='http://10.0.2.6/mutillidae')
+    links = []
+    links.append('http://10.0.2.6/mutillidae')
+
+    ScanObj = XSS(url=links, payload_file='xss_payloads.txt')
+    ScanObj.initiateEngine()
