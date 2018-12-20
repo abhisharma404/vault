15027aad0ea4873b37daa32e834e7b85c9e02370
Sohail Alvi <sohailalvi2236@gmail.com>
2018-12-20 19:48:15 +0530

    Issue related due to deadlock resolved

--- src/lib/crawler/spider.py
+++ src/lib/crawler/spider.py
@@ -3,8 +3,10 @@
 
 import urllib.request
 import urllib.response
+import urllib.error
 from . import finder as fobj
 from . import imutil as imu
+import time
 
 
 class Spider:
@@ -30,40 +32,37 @@ class Spider:
     @staticmethod
     def boot():
         imu.create_project_dir(Spider.project_name)
-        imu.create_data_files(Spider.project_name, Spider.base_url)#Entering first url
-        #print('Writing First Url' + Spider.base_url)
+        imu.create_data_files(Spider.project_name, Spider.base_url)
         Spider.queue = imu.file_to_set(Spider.queue_file)
-        #print(str(len(Spider.queue)))
         Spider.crawled = imu.file_to_set(Spider.crawled_file)
 
 
     @staticmethod
     def crawl_page(thread_name, page_url):
-        ############ TO CHECK IF LINK has been crawled  #######
         if page_url not in Spider.crawled:
             print(thread_name + ' now crawling ' + page_url)
             print('Queue ' + str(len(Spider.queue)) + ' | Crawled  ' + str(len(Spider.crawled)))
             Spider.add_links_to_queue(Spider.gather_links(page_url))
-            Spider.queue.remove(page_url)#<<<<<<<<<<<<<<<<___<<<<<<<
+            Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
             Spider.update_files()
 
 
     @staticmethod
     def gather_links(page_url):
-        html_string = ''
         try:
-            req = urllib.request.Request(page_url, headers ={'User-Agent':'Mozilla/5.0'})
-            con = urllib.request.urlopen(req)
-            html_string=con.read().decode("utf-8")
-            obj = fobj.Linkfinder(Spider.base_url)
-            obj.feed(html_string)
-            return obj.links_obtained()
+            if not page_url.lower().startswith('ftp') or not page_url.lower().startswith('file'):
+                time.sleep(1)
+                req = urllib.request.Request(page_url, headers ={'User-Agent':'Mozilla/5.0'})
+                con = urllib.request.urlopen(req)
+                html_string=con.read().decode("utf-8")
+                obj = fobj.Linkfinder(Spider.base_url)
+                obj.feed(html_string)
+                return obj.links_obtained()
 
-        except Exception as e:
-            print("Exception Occured!!!\n"+ str(e))
+        except urllib.error.URLError as e:
+            print("Exception Occured!!!\n" + str(e))
             return ''
-        pass
 
 
     @staticmethod
@@ -73,7 +72,6 @@ class Spider:
                 continue
             if Spider.domain_name != imu.get_domain_name(url):
                 continue
-            print(url)
             Spider.queue.add(url)
 
 
